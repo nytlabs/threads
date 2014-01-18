27b4521240593fd2643cf60919f17ec34d236bff
Alexis Lloyd <alexislloyd@gmail.com>
2014-01-17 21:47:19 -0500

    adding alt versions

--- display/threads.js
+++ display/threads.js
@@ -1,9 +1,11 @@
-$(document).ready(function() {
 
-	$.getJSON( "../threads.json", function( data ) {
+
+function populateThreads(jsonFile) {
+	$('#nav').nextAll().remove();
+	$.getJSON( jsonFile, function( data ) {
 		var items = [];
 		$.each( data, function( key, val ) {
-		  var item = "<h2>";
+		  var item = "<div class='item'><h2><span class='entypo-down-dir'></span>";
 		  $.each( val["keywords"], function( k, v ) {
 		  	item += "<div class='tag'>" + k + "</div>";
 		  });
@@ -14,12 +16,20 @@ $(document).ready(function() {
 		  	date = new Date(v["published"]["$date"])
 		  	item += "<li><span class='date'>"+("0" + (date.getMonth() + 1)).slice(-2)+"/"+("0" + date.getDate()).slice(-2)+"/"+date.getFullYear()+"</span><a href='"+v["_id"]+"'>"+v["headline"]+"</a></li>";
 		  });
-		  item += "</ul>";
+		  item += "</ul></div>";
 		  $('#wrapper').append(item);
 		});
-	});
 
-});
+		$(".entypo-down-dir").on( "click", function() {
+		  $(this).parent().parent().children('ul').toggle();
+		  $(this).toggleClass('open')
+		});
+	});
+}
+
+
+
+
 
 function custom_sort(a, b) {
     return a["published"]["$date"] - b["published"]["$date"];
